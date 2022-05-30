1bab5736bb965cc6971524ae65d582b314bbce01
lovkesh patel <79805511+lovkesh07@users.noreply.github.com>
2022-05-30 11:02:59 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -112,8 +112,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "40px 10px";
+    document.getElementById("navbar").style.padding = "40px 10px" ;
     document.getElementById("logo1").style.fontSize = "15px";
+    document.getElementById("logo1").style.color = "white";
   } else {
     document.getElementById("navbar").style.padding = "40px 10px";
     document.getElementById("logo1").style.fontSize = "25px ";
